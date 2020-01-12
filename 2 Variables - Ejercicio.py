412c69da356c5cba6d5738a92ed30b2d6cea8e59
Programa Resuelto <58753269+Programa-Resuelto@users.noreply.github.com>
2020-01-12 17:18:05 -0500

    Add files via upload

---
+++ 2 Variables - Ejercicio.py
@@ -0,0 +1,16 @@
+#Escribe tu nombre en la línea 3.
+#Guarda tu nombre en la variable miNombre.
+
+#Escribe el nombre de tu mejor amigo en la línea 6.
+#Guarda ese nombre en la variable miMejorAmigo.
+
+#Escribe el nombre del lugar en el que conociste a tu mejor amigo en la línea 9.
+#Guarda ese nombre en la variable lugarEnQueNosConocimos
+
+#Corre el código. Debería imprimir tu nombre, el de tu mejor amigo y el lugar en que se conocieron.
+print("Mi nombre es:")
+print(miNombre)
+print("Mi mejor amigo es:")
+print(miMejorAmigo)
+print("Nos conocimos en:")
+print(lugarEnQueNosConocimos)
