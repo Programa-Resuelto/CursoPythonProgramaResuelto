8c4a2b12fc446d7be1a5f94159e5de6fc54593d2
Programa Resuelto <58753269+Programa-Resuelto@users.noreply.github.com>
2020-01-13 00:40:43 -0500

    Add files via upload

---
+++ 4 Variables como números - Ejercicio.py
@@ -0,0 +1,24 @@
+#Estás con tus amigos y quieren comprar entre todos una botella de Tequila.
+#La botella de Tequila tiene un precio de 65 dólares.
+#En la línea 4, crea una variable llamada dineroTotal y asígnale el número 0.
+
+#Tú tienes 14.50 dólares.
+#En la línea 7, crea una variable llamada miDinero con el dinero que tienes.
+
+#Tu amigo Luis tiene 17 dólares.
+#En la línea 10, crea una variable llamada dineroLuis con el dinero que él tiene.
+
+#Tu amiga Valentina tiene 12.15 dólares.
+#En la línea 13, crea una variable llamada dineroValentina con el dinero que ella tiene.
+
+#Tu amigo Daniel tiene 20 dólares.
+#En la línea 16, crea una variable llamada dineroDaniel con el dinero que él tiene.
+
+#Ahora, en la línea 20, vamos a contar el dinero que tienen entre todos.
+#Actualiza la variable dineroTotal para que sea igual a la suma del dinero de todos.
+#No uses los números directamente en la suma, sino las variables.
+
+#En la línea 22, imprime la variable dineroTotal para saber cuánto dinero tienen.
+
+#¿Tienen suficiente dinero para comprar la botella de Tequila?
+#En la línea 25 imprime un mensaje para decirle a tus amigos si pueden comprar la botella o no.
---
+++ 5 Actualizar Variables - Ejercicio.py
@@ -0,0 +1,25 @@
+#Vas a ir de compras.
+#Inicialmente tienes 50 dólares disponibles para gastar.
+#En la línea 4, asigna el dinero que tienes a la variable dineroDisponible.
+
+#Compraste una camiseta de 12.30 dólares.
+#En la línea 7, actualiza con el operador -= la variable dineroDisponible.
+
+#Imprime en la línea 9 el dinero que te queda.
+
+#Ahora compraste una lámpara para decorar tu casa. Te costó 30 dólares.
+#En la línea 12, usa el operador correspondiente para actualizar el dinero que te queda.
+
+#En la línea 14, imprime el dinero que te queda.
+
+#No te queda mucho dinero. Llamaste a un amigo que te prestó 20 dólares.
+#Añade, en la línea 17 y con el operador +=, esos 20 dólares a tu dinero disponible.
+
+#¿Cuánto dinero tienes ahora? Imprímelo por pantalla en la línea 19.
+
+#¡Te alcanza para unas gafas nuevas de 25 dólares! ¡Cómpralas!
+#En la línea 23, actualiza tu variable dineroDisponible para representar esta compra.
+#Luego, en la línea 24, imprime el dinero que te sobró.
+
+
+#Si te quedaron 2.70 dólares, no solo hiciste bien el programa, ¡sino que puedes comprarte un postre!
