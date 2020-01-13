09923385206ad209dd8894c1062ecbecb8d8310d
Programa Resuelto <58753269+Programa-Resuelto@users.noreply.github.com>
2020-01-12 19:21:31 -0500

    Add files via upload

---
+++ 3 Números - Ejercicio.py
@@ -0,0 +1,27 @@
+#Estás diseñando un juego. Tienes que darle valores a las armas que se usan.
+#Aquí verás las dos armas que se usan en el juego.
+arma1 = "Espada"
+arma2 = "Arco"
+#Ahora tienes que asignarle un número a las características del arma.
+#En la línea 8 escribe un número float entre 0.0 y 10.0 que represente el daño de la espada.
+#Guarda ese número en la variable dañoEspada.
+
+#En la línea 11 escribe un número int entre 1 y 10 que represente la resistencia de la espada.
+#Guarda ese número en la variable resistenciaEspada.
+
+#En la línea 14 escribe un número float entre 0.0 y 10.0 que represente el daño del arco.
+#Guarda ese número en la variable dañoArco.
+
+#En la línea 17 escribe un número int entre 1 y 10 que represente la resistencia del arco.
+#Guarda ese número en la variable resitenciaArco.
+
+#Corre el código. Debería imprimir las armas y sus características.
+print("Arma 1: " + arma1)
+print("Daño:", dañoEspada)
+print("Resistencia:", resistenciaEspada)
+print()
+print("Arma 2: " + arma2)
+print("Daño:", dañoArco)
+print("Resistencia:", resistenciaArco)
+#No te preocupes si no entiendes cómo usé la función print en este ejercicio.
+#Aprenderás a imprimir texto y variables a la vez en los siguientes videos.
