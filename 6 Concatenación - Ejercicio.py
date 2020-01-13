d999119069d8b929a740a0bb2d2da3d66df1f8e5
Programa Resuelto <58753269+Programa-Resuelto@users.noreply.github.com>
2020-01-13 02:27:42 -0500

    Add files via upload

---
+++ 6 Concatenación - Ejercicio.py
@@ -0,0 +1,16 @@
+saludo = "Hola"
+#Python necesita tu nombre para saludarte.
+#En la línea 4, escribe tu nombre en una varible de tipo String llamada nombre.
+
+#En la línea 8, concatena el saludo y el nombre.
+#Guarda esa concatenación en la variable saludoCompleto.
+#Recuerda separar el saludo y el nombre con una coma y un espacio (", ").
+
+#En la línea 11, guarda tu edad en una variable llamada edad.
+#Tu edad debe ser guardada en una variable de tipo int.
+
+#Corre el programa. Verás que hay un TypeError.
+textoEdad = "En un año tendrás " + edad+1 + " años."
+print(saludoCompleto + ". " + textoEdad)
+#Prueba a usar la función str() para arreglar la variable textoEdad.
+#Luego corre el programa y confirma que la información que te dice sea correcta.
