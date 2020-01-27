105f861d79eace359742731b66e517591ba3f10f
Programa Resuelto <58753269+Programa-Resuelto@users.noreply.github.com>
2020-01-26 21:23:11 -0500

    Add files via upload

---
+++ 7 Función Input - Ejercicio.py
@@ -0,0 +1,34 @@
+#Vamos a hacer un programa que lea la informació de una persona y la imprima.
+#Primero, vamos a preguntarle su nombre.
+#En la línea 6 crea la variable 'nombre'.
+#Asígnale a esa variable la función input() con un mensaje entre los paréntesis.
+#El mensaje debe decir "¿Cuál es tu nombre?: ".
+
+#Bien. Ahora la edad. Crea una variable en la línea 11 que se llame 'edad'.
+#Asígnale un input a la variable 'edad', con un mensaje entre los paréntesis.
+#El mensaje debe decir "¿Cuántos años tienes?: ".
+#Encierra la función input entre la función int() para hacer entero el número.
+
+#Ahora preguntaremos los gustos. Crearemos varias variables.
+#En la línea 15 crea la variable 'comidaFavorita'. Asígnale un input.
+#Escribe el mensaje "¿Cuál es tu comida favorita?: " dentro del input.
+
+#Ahora haremos lo mismo con el tipo de música favorita.
+#En la línea 18, crea la variable 'musicaFavorita', con un input y un mensaje.
+
+#En la línea 20, crea la variable 'tipoDePeliculasFavorito'.
+#También asígnale un input y un mensaje acorde.
+
+#Ahora completa los prints de las líneas 23-28 para mostrar la información.
+#Mira las pistas en la líneas 30-34 si lo necesitas.
+print()
+print("Nombre:")
+print("Edad:")
+print("Comida Favorita:")
+print("Música Favorita:")
+print("Tipo de Películas Favorito:")
+#Deja el primer print() vacío. Servirá como un salto de línea.
+#El segundo print imprime "Nombre:" pero no muestra el nombre ingresado.
+#Usa una coma para separar ese texto de la variable 'nombre'.
+#Haz lo mismo con los demás textos.
+#También puedes concatenar, ¡pero ten cuidado con la variable 'edad'!.
